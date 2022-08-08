35b7e970682ab0ea8cfb26cd9183884b83958b35
Jamoussi Mohamed Amine <djama@Jamoussis-MacBook-Air.local>
2022-08-08 12:33:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Navjan from './compo/Navjan';
+import Aboutjan from './compo/Aboutjan';
+import Projectjan from './compo/Projectjan';
+import Contactjan from './compo/Contactjan';
+import Footerjan from './compo/Footerjan';
+
+function App() {
+  return (
+    <>
+    
+      <Navjan/>    
+      <Aboutjan/>
+      <Projectjan/>
+      <Contactjan/>
+      <Footerjan/>
+    
+    
+    
+    
+    
+    </>
+    
+  );
+}
+
+export default App;
